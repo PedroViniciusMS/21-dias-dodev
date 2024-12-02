e4f69d545dd97c3c621ab704c02b187bc7447301
Pedro Vinicius <pedroviniciusm@hotmail.com>
2024-12-02 17:56:21 -0300

    complementando exercico 04 tabuada, usando FOR

--- Dia 6/Lista de exercícios 01/exercicio 04.js
+++ Dia 6/Lista de exercícios 01/exercicio 04.js
@@ -1,2 +1,13 @@
-//Exercicio 04 # Dia 06
+//Exercicio 04 # Dia 06 (tabuada)
 
+//Entrada 
+    let escolha = parseInt(prompt('Digite um número de sua preferência:'))
+    
+//Criando tabuada   
+//For
+for(let tabuada = escolha; tabuada <= escolha + 2; tabuada++){
+        console.log('Tabuada do número: ' + tabuada);
+    for(let algoritimo = 0; algoritimo <= 10; algoritimo++){
+        console.log(tabuada + " x " + algoritimo + " = " + (tabuada * algoritimo))
+    }
+}
